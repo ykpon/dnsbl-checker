80d42ae2c71f03181addd957c1e15909b1f0316d
ykpon <ykpon@neadekvat.ru>
2020-11-10 17:09:58 +0300

    Lint. Comment for Config struct

--- config/config.go
+++ config/config.go
@@ -5,17 +5,18 @@ import (
 	"os"
 )
 
-type config struct {
+// Config struct for file config.json
+type Config struct {
 	TelegramBotToken      string `json:"TELEGRAM_BOT_TOKEN"`
 	TelegramChannelChatID int64  `json:"TELEGRAM_CHANNEL_CHAT_ID"`
 }
 
 // LoadConf func ...
-func LoadConf() config {
+func LoadConf() Config {
 	file, _ := os.Open("resources/config.json")
 	defer file.Close()
 	decoder := json.NewDecoder(file)
-	configuration := config{}
+	configuration := Config{}
 	err := decoder.Decode(&configuration)
 	if err != nil {
 		panic(err)
